42d4e5dae1ce4c7f5a649e08b9c43c3d0670b3cd
Saahith Janapati <itzsaahith01@gmail.com>
2023-08-25 18:58:01 -0400

    migrate latest changes

--- src/CourseResultComponent.js
+++ src/CourseResultComponent.js
@@ -1,85 +1,81 @@
 import React, { useState } from 'react'
 
+// TODO: move this to an environment variable that gets automatically changed
+const latestSem = "1238"; // var used to get link to SIS
 
-/*
-name
-level
-catalog_number
-class_number
-subject
-*/
+function mapNumberToSeasonWithMiddleDigits(number) {
+  if (typeof number !== 'number' || number < 1000 || number > 9999) {
+      return "N/A";
+  }
+
+  const lastDigit = number % 10;
+  const middleDigits = Math.floor((number / 10) % 100);
+  let result = '';
+  switch (lastDigit) {
+      case 8:
+          result = "Fall ";
+          break;
+      case 2:
+          result = "Spring ";
+          break;
+      case 1:
+          result = "Winter ";
+          break;
+      case 6:
+          result = "Summer ";
+          break;
+      default:
+          result = "";
+          break;
+  }
+
+  result += "'" + middleDigits.toString();
+  return result;
+}
 
 const CourseResultComponent = (props) => {
-  const [isActive] = useState(false);
-  const [sessions] = useState([]);
-  const [isLoading] = useState(false);
 
-
-  const handleClick = async () => {
-    console.log("clicked");
-    // setIsActive(!isActive);
-    // setIsLoading(true);
-    // console.log("clicked");
-
-    // const response = await fetch("/detailed_info", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({ catalog_number: props.catalog_number, mnemonic: props.mnemonic}),
-    // });
-    // const data = await response.json();
-    // setIsLoading(false);
-
-    // if (data && Array.isArray(data)) {
-    //   const sessions = data.map((result, index) => (
-    //     <div key={index}>
-    //       <SessionResultComponent 
-    //       instructor = {result.instructor}
-    //       type = {result.type}
-    //       location = {result.location}
-    //       enrollment_capacity = {result.enrollment_capacity}
-    //       current_enrolled = {result.current_enrolled}
-    //       waitlist_capacity = {result.waitlist_capacity}
-    //       current_waitlisted = {result.current_waitlisted}
-    //       days = {result.days}
-    //       start_time = {result.start_time}
-    //       end_time = {result.end_time}
-    //       />
-    //     </div>
-    //   ));
-    //   console.log(sessions);
-    //   setSessions(sessions);
-    // }
+  const handleMoreLikeThisButtonClick = () => {
+      props.onMoreLikeThisClick(props.mnemonic, props.catalog_number);
   }
-  
-  
+
+  const getSisLink = () => {
+    if(props.strm.toString() === latestSem){
+      //search link
+      return `https://sisuva.admin.virginia.edu/psp/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_Main?catalog_nbr=${props.catalog_number}&subject=${props.mnemonic}`
+    }
+    //share link
+    return `https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_DETAILS.FieldFormula.IScript_Main?institution=UVA01&term=${props.strm}&class_nbr=${props.class_number}`
+  }
+
   return (
     <React.Fragment>
       <div className="accordion" style={{paddingBottom:'20px'}}>
         <div className="accordion-item">
-          <div 
-          className="accordion-title"
-          onClick={handleClick}>
+        <a href={getSisLink()} target="_blank">
+          <div className="accordion-title">
             <div>
-              {props.subject} {props.catalog_number}: {props.name} ({props.level})
+              {props.mnemonic} {props.catalog_number}: {props.name} ({props.level})
             </div>
-            <div>{isActive ? '-' : '+'}</div>
           </div>
+          </a>
+
           <div className="accordion-content">
-            <div style={{fontWeight:'bold'}}>Similarity Score: {props.similarity_score}</div>
-            <div style={{fontWeight:'bold'}}>Credits: {props.credits}</div>
-            <div>{props.description}</div>
+          <body>{props.description}</body>
+
+    <div>
+            <body style={{fontWeight:'bold'}}>Similarity Score: {props.similarity_score.toFixed(3)}</body>
+            <body style={{fontWeight:'bold'}}>Credits: {props.credits}</body>
+
+            <body style={{fontWeight:'bold'}}>Latest Offerring: {mapNumberToSeasonWithMiddleDigits(props.strm)}</body>
+            </div>
+        
+            <button onClick={handleMoreLikeThisButtonClick}>More like this</button>
           </div>
-        </div>
-        {isActive && <div className="accordion-content">
-        <div>{isLoading && <h5>Detailed Info not Implemented Yet</h5>}</div> 
-        {sessions} 
-        </div>}
+        </div>        
       </div>
     </React.Fragment>
   );
-  
 }
 
 export default CourseResultComponent
