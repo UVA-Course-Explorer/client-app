3ad33ae501bacf5932df9c7e5943f0ed486deb65
sidlakkoju <siddharth.lakkoju@gmail.com>
2023-09-10 13:51:08 -0400

    Added new light model colors

--- src/Modal.js
+++ src/Modal.js
@@ -20,27 +20,28 @@ const Modal = () => {
 
 
       <button onClick={openModal}>Open Modal</button>
-        <Modal
-          isOpen={isModalOpen}
-          onRequestClose={closeModal}
-          contentLabel="Example Modal"
-          className={{
-            base: 'fade',
-            afterOpen: 'fade-enter',
-            beforeClose: 'fade-exit',
-          }}
-          overlayClassName={{
-            base: 'fade-overlay',
-            afterOpen: 'fade-overlay-enter',
-            beforeClose: 'fade-overlay-exit',
-          }}
-        >
-          <h2 className="modal-content">Modal Content</h2>
-          <p className="modal-content">This is the content of the modal.</p>
-          <button onClick={closeModal} className="modal-button">
-            Close Modal
-          </button>
-        </Modal>
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+        className={{
+          base: 'fade',
+          afterOpen: 'fade-enter',
+          beforeClose: 'fade-exit',
+        }}
+        overlayClassName={{
+          base: 'fade-overlay',
+          afterOpen: 'fade-overlay-enter',
+          beforeClose: 'fade-overlay-exit',
+        }}
+      >
+        <h2 className="modal-content">Modal Content</h2>
+        <p className="modal-content">This is the content of the modal.</p>
+        <button onClick={closeModal} className="modal-button">
+          Close Modal
+        </button>
+      </Modal>
 
 
     </div>
