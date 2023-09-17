7a6b6dc4cd3da4a90c902e25eaf9e469c57bc964
Saahith Janapati <itzsaahith01@gmail.com>
2023-09-17 05:25:15 -0400

    remove unnecessary imports

--- src/CatalogPage.js
+++ src/CatalogPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link , useParams} from 'react-router-dom';
+import { BrowserRouter as useParams} from 'react-router-dom';
 
 function CatalogPage() {
     const { department } = useParams();
