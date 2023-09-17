5f1897bae2b32cc4686ffcfc0c19d55340630904
Saahith Janapati <itzsaahith01@gmail.com>
2023-09-17 05:32:28 -0400

    remove dependency

--- src/CatalogPage.js
+++ src/CatalogPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as useParams} from 'react-router-dom';
+import { BrowserRouter as Router, Route, useParams} from 'react-router-dom';
 
 function CatalogPage() {
     const { department } = useParams();
